5ddafb6ffe6c31f85433017abc79633a1637038f
DOGMA25 <143981617+12210036@users.noreply.github.com>
2024-06-20 14:09:18 -0600

    Update script.js

--- fnms/script.js
+++ fnms/script.js
@@ -1,10 +1,11 @@
 function getRandomInt(max) {
   return Math.floor(Math.random() * max);
 }
-x = getRandomInt(20000000000000001);
+const x = getRandomInt(20000000000000001);
 alert(x);
 
 function holi(){
+ 
   
   var input = document.getElementById("searchInput");
   
