9f6705cb8a2970f33648f2d986cc61013ee84497
DOGMA25 <143981617+12210036@users.noreply.github.com>
2024-06-20 12:14:45 -0600

    a

---
+++ script.js
@@ -0,0 +1,15 @@
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+x = getRandomInt(20000000000000001);
+alert(x);
+
+function holi(){
+  
+  var input = document.getElementById("searchInput");
+  
+  if (x == input){
+  location.href ="index3.html";
+  }
+
+}
